55aca07dfaa598529c7903084781f6a74fbdcda8
Chris Birchall <chris.birchall@gmail.com>
2016-01-19 00:42:01 +0000

    Add Primer CSS

---
+++ scripts/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Copy/build the required files to the dist/ directory
+
+project_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
+src=$project_root/src
+dist=$project_root/dist
+
+mkdir -p $dist
+cp $src/index.html $dist
+cp -R $src/components $dist
+
+webpack --config ./webpack.conf.js
---
+++ scripts/start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Start the webpack dev server
+
+project_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
+src=$project_root/src
+dist=$project_root/dist
+
+mkdir -p $dist
+cp $src/index.html $dist
+cp -R $src/components $dist
+
+webpack-dev-server --config ./webpack.conf.js --content-base $dist/
