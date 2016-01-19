cba48c73619065c50aae5ebc18cdd41416a88ed7
Chris Birchall <chris.birchall@gmail.com>
2016-01-19 22:47:36 +0000

    Load PRs for each repo
    * Add total PRs counters to orgs
    * Add a Loading... spinner (just text for now)

--- src/js/components/dashboard.react.js
+++ src/js/components/dashboard.react.js
@@ -5,7 +5,8 @@ export default class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      orgs: {}
+      orgs: {},
+      loadingRepos: true
     };
   }
 
@@ -17,6 +18,28 @@ export default class Dashboard extends React.Component {
     this.getPageOfRepos(1);
   }
 
+  getAllOpenPRs() {
+    Object.keys(this.state.orgs).forEach((org) => {
+      this.getOpenPRsForOrg(this.state.orgs[org]);
+    })
+  }
+
+  getOpenPRsForOrg(org) {
+    var t = this;
+    org.repos.forEach((repo) => {
+      this.props.github.get('/repos/'+org.org.login+'/'+repo.name+'/pulls?per_page=10&state=open&sort=updated&direction=desc').done((data) => {
+        if (data.length > 0) {
+          console.log('Adding open PRs for ' + repo.full_name);
+          console.log(data);
+          repo.openPRs = data;
+          t.setState({ orgs: t.state.orgs });
+        } else {
+          // TODO remove any repos with no open PRs
+        }
+      });
+    })
+  }
+
   getPageOfRepos(page) {
     var t = this;
     this.props.github.get('/user/repos?per_page=100&page='+page).done((data) => {
@@ -25,6 +48,11 @@ export default class Dashboard extends React.Component {
         console.log('Adding page of repos');
         t.setState({ repos: t.addRepos(data) });
         t.getPageOfRepos(page + 1);
+      } else {
+        t.setState({ loadingRepos: false });
+
+        // Now we have all orgs and repos, start getting open PRs for repos
+        t.getAllOpenPRs();
       }
     });
   }
@@ -34,8 +62,10 @@ export default class Dashboard extends React.Component {
     pageOfRepos.forEach((repo) => {
       var org = repo.owner;
       var orgAndRepos = orgs[org.login] || { org: org, repos: [] };
-      orgAndRepos.repos.push(repo);
-      orgs[org.login] = orgAndRepos;
+      if (repo.open_issues_count > 0) {
+        orgAndRepos.repos.push(repo);
+        orgs[org.login] = orgAndRepos;
+      }
     });
     orgs;
   }
@@ -46,7 +76,7 @@ export default class Dashboard extends React.Component {
         <div>TODO filters go here</div>
         <div className="columns">
           <div className="one-fifth column">
-            <OrgsList github={this.props.github} orgs={this.state.orgs} />
+            <OrgsList github={this.props.github} orgs={this.state.orgs} loadingRepos={this.state.loadingRepos} />
           </div>
           <div className="four-fifths column">
             TODO repo list goes here
--- src/js/components/orgsList.react.js
+++ src/js/components/orgsList.react.js
@@ -6,17 +6,46 @@ export default class OrgsList extends React.Component {
     this.state = {};
   }
 
+  totalOpenPRs(repos) {
+    var total = repos.reduce((acc, repo) => { 
+      var openPRs = 0;
+      if (repo.openPRs) 
+        openPRs = repo.openPRs.length;
+      return acc + openPRs;
+    }, 0);
+    console.log(total);
+    return total;
+  }
+
   render() {
     var orgNodes = Object.keys(this.props.orgs).map((login) => {
       var orgAndRepos = this.props.orgs[login];
       return (
-        <a className="menu-item" key={login}><img className="avatar avatar-small" width="20" height="20" src={orgAndRepos.org.avatar_url} />{login}</a>
+        <a className="menu-item" key={login}><img className="avatar avatar-small" width="20" height="20" src={orgAndRepos.org.avatar_url} />
+          <span className="counter">{this.totalOpenPRs(orgAndRepos.repos)}</span>
+          {login}
+        </a>
       );
     })
     return (
       <nav className="menu">
         {orgNodes}
+        <LoadingSpinner loading={this.props.loadingRepos} />
       </nav>
     );
   }
 }
+
+class LoadingSpinner extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    if (this.props.loading)
+      return <div>Loading orgs...</div>;
+    else
+      return null;
+  }
+}
+
